456a27d4c7eab4f2e9eb12bf0d5f0ef48719250b
Luiz Perez <lguilhermepperez@hotmail.com>
2024-01-30 17:04:34 +1000

    Week11

---
+++ Studies/Week11/main.js
@@ -0,0 +1,11 @@
+import {user} from './user.js';
+
+const student = user('Peter parker', 18);
+student.printName();
+student.printAge();
+
+const element = document.getElementById('target');
+
+const html = `The student name is ${student.name}, the age is ${student.age}`;
+
+element.innerHTML = html;
